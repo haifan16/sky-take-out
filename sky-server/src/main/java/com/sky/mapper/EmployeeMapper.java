10724f508e1b067acbb27ab213908f36742bbcc3
haifan <1504948417@qq.com>
2023-12-13 03:26:39 +0800

    新增员工业务代码开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -21,7 +21,7 @@ public interface EmployeeMapper {
      * @param employee
      */
     @Insert("insert into employee (name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user) " +
-            "values" +
-            "(#{name}, #{username}, #{password}, #{phone}, #{sex}, #{id_number}, #{create_time}, #{update_time}, #{create_user}, #{update_user})")
+            "values " +
+            "(#{name}, #{username}, #{password}, #{phone}, #{sex}, #{idNumber}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
     void insert(Employee employee);
 }
